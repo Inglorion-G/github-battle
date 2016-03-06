31a6ed239f66999ac90bd7157fec4c24af0e172c
George Groh <george.groh@gmail.com>
2016-03-05 22:29:13 -0800

    Multiple components, pure functions, props

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+    name: 'George Groh',
+    username: 'Inglorion-G',
+    image: 'https://avatars2.githubusercontent.com/u/2111262'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+    render: function () {
+        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+    }
+});
+
+var ProfileLink = React.createClass({
     render: function () {
         return (
-            <div> Hello World </div>
+            <div>
+                <a href={'https://www.github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function () {
+        return (
+            <div>{this.props.name}</div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.user.image} />
+                <ProfileName name={this.props.user.name} />
+                <ProfileLink username={this.props.user.username} />
+            </div>
         )
     }
 });
 
 ReactDOM.render(
-    <HelloWorld />,
+    <Avatar user={USER_DATA} />,
     document.getElementById('app')
 );
